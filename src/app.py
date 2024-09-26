6c5aecf18094e6b95386217a49258b37f6f779be
jronald88 <156713858+jronald88@users.noreply.github.com>
2024-09-26 05:05:39 +0000

    first

--- src/app.py
+++ src/app.py
@@ -1,4 +1,281 @@
-from utils import db_connect
-engine = db_connect()
+import os
+import pandas as pd 
+import numpy as np
 
-# your code here
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pickle
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import tree
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import StandardScaler 
+from sklearn import metrics
+
+from flask import Flask, request, render_template
+from pickle import load
+
+df = pd.read_csv('/workspaces/ML_flask/models/heart_2020_cleaned.csv')
+print(df.head())
+
+print(df.describe().T.style.set_properties(**{'background-color': 'grey','color': 'white','border-color': 'white'}))
+print(df.info())
+print(df.nunique())
+df =  df[df.columns].replace({'Yes':1, 'No':0, 'Male':1,'Female':0,'No, borderline diabetes':'0','Yes (during pregnancy)':'1' })
+df['Diabetic'] = df['Diabetic'].astype(int)
+
+fig, ax = plt.subplots(figsize = (13,6))
+
+ax.hist(df[df["HeartDisease"]==1]["Sex"], bins=15, alpha=0.5, color="red", label="HeartDisease")
+ax.hist(df[df["HeartDisease"]==0]["Sex"], bins=15, alpha=0.5, color="#fccc79", label="Normal")
+
+ax.set_xlabel("Sex")
+ax.set_ylabel("Frequency")
+
+fig.suptitle("Distribution of Cases with Yes/No heartdisease according to Sex")
+
+ax.legend()
+plt.savefig("./models/distribution_of_cases_h_disease.jpg")
+
+
+plt.figure(figsize = (13,6))
+sns.countplot( x= df['Race'], hue = 'HeartDisease', data = df, palette = 'YlOrBr')
+plt.xlabel('Race')
+plt.legend()
+plt.ylabel('Frequency')
+plt.savefig("./models/race_vs_freq.jpg")
+
+plt.figure(figsize = (13,6))
+sns.countplot(x = df['AgeCategory'], hue = 'HeartDisease', data = df, palette = 'YlOrBr')
+fig.suptitle("Distribution of Cases with Yes/No heartdisease according to AgeCategory")
+plt.xlabel('AgeCategory')
+plt.legend(['Normal','HeartDisease'])
+plt.ylabel('Frequency')
+plt.savefig("./models/heart_age.jpg")
+
+corr = df.select_dtypes(include=np.number).corr()
+mask = np.triu(np.ones_like(corr, dtype = bool))
+plt.figure(figsize = (14,7))
+sns.heatmap(corr, annot = True, cmap = 'YlOrBr', mask = mask)
+plt.savefig("./models/heatmap.jpg")
+
+fig, ax = plt.subplots(figsize = (13,5))
+sns.kdeplot(df[df["HeartDisease"]==1]["BMI"], alpha=0.5,shade = True, color="red", label="HeartDisease", ax = ax)
+sns.kdeplot(df[df["HeartDisease"]==0]["BMI"], alpha=0.5,shade = True, color="#fccc79", label="Normal", ax = ax)
+plt.title('Distribution of Body Mass Index', fontsize = 18)
+ax.set_xlabel("BodyMass")
+ax.set_ylabel("Frequency")
+ax.legend();
+plt.savefig("./models/kde_HD_BMI.jpg")
+
+#Train-Test Split
+
+#Select Features
+features = df.drop(columns =['HeartDisease'], axis = 1)
+
+#Select Target 
+target = df['HeartDisease']
+
+# Set Training and Testing Data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(features, target, shuffle = True, test_size = 0.2, random_state = 42)
+
+
+print('Shape of training feature:', X_train.shape)
+print('Shape of testing feature:', X_test.shape)
+print('Shape of training label:', y_train.shape)
+print('Shape of training label:', y_test.shape)
+
+transformer = make_column_transformer(
+    (OneHotEncoder(), ['AgeCategory', 'Race', 'GenHealth']),
+    remainder='passthrough')
+
+# Encode training data 
+transformed_train = transformer.fit_transform(X_train)
+transformed_train_data = pd.DataFrame(transformed_train, columns=transformer.get_feature_names_out())
+
+# Concat the two tables
+transformed_train_data.reset_index(drop=True, inplace=True)
+X_train.reset_index(drop=True, inplace=True)
+X_train = pd.concat([transformed_train_data, X_train], axis=1)
+
+# Remove old columns
+X_train.drop(['AgeCategory', 'Race', 'GenHealth'], axis = 1, inplace = True)
+
+
+
+# Encode test data 
+transformed_test = transformer.fit_transform(X_test)
+transformed_test_data = pd.DataFrame(transformed_test, columns=transformer.get_feature_names_out())
+
+# Concat the two tables
+transformed_test_data.reset_index(drop=True, inplace=True)
+X_test.reset_index(drop=True, inplace=True)
+X_test = pd.concat([transformed_test_data, X_test], axis=1)
+
+
+# Remove old columns
+X_test.drop(['AgeCategory', 'Race', 'GenHealth'], axis = 1, inplace = True)
+
+scaler = StandardScaler()
+
+# Scale trainint data
+X_train = scaler.fit_transform(X_train)
+
+# Scale test data
+X_test = scaler.fit_transform(X_test)
+
+def evaluate_model(model, x_test, y_test):
+    from sklearn import metrics
+
+    # Predict Test Data 
+    y_pred = model.predict(x_test)
+
+    # Calculate accuracy, precision, recall, f1-score, and kappa score
+    acc = metrics.accuracy_score(y_test, y_pred)
+    prec = metrics.precision_score(y_test, y_pred)
+    rec = metrics.recall_score(y_test, y_pred)
+    f1 = metrics.f1_score(y_test, y_pred)
+    kappa = metrics.cohen_kappa_score(y_test, y_pred)
+
+    # Calculate area under curve (AUC)
+    y_pred_proba = model.predict_proba(x_test)[::,1]
+    fpr, tpr, _ = metrics.roc_curve(y_test, y_pred_proba)
+    auc = metrics.roc_auc_score(y_test, y_pred_proba)
+
+    # Display confussion matrix
+    cm = metrics.confusion_matrix(y_test, y_pred)
+
+    return {'acc': acc, 'prec': prec, 'rec': rec, 'f1': f1, 'kappa': kappa, 
+            'fpr': fpr, 'tpr': tpr, 'auc': auc, 'cm': cm}
+
+print(X_train)
+
+# Building a model using KNeighborsClassifier 
+knn = KNeighborsClassifier(n_neighbors = 5)
+
+knn.fit(X_train, y_train)
+
+
+# Evaluate Model
+knn_eval = evaluate_model(knn, X_test, y_test)
+
+# Print result
+print('Accuracy:', knn_eval['acc'])
+print('Precision:', knn_eval['prec'])
+print('Recall:', knn_eval['rec'])
+print('F1 Score:', knn_eval['f1'])
+print('Cohens Kappa Score:', knn_eval['kappa'])
+print('Area Under Curve:', knn_eval['auc'])
+print('Confusion Matrix:\n', knn_eval['cm'])
+
+# Building Decision Tree model 
+clf = tree.DecisionTreeClassifier(random_state=0)
+clf.fit(X_train, y_train)
+
+# Evaluate Model
+clf_eval = evaluate_model(clf, X_test, y_test)
+
+# Print result
+print('Accuracy:', clf_eval['acc'])
+print('Precision:', clf_eval['prec'])
+print('Recall:', clf_eval['rec'])
+print('F1 Score:', clf_eval['f1'])
+print('Cohens Kappa Score:', clf_eval['kappa'])
+print('Area Under Curve:', clf_eval['auc'])
+print('Confusion Matrix:\n', clf_eval['cm'])# Evaluate Model
+
+# Intitialize figure with two plots
+fig, (ax1, ax2) = plt.subplots(1, 2)
+fig.suptitle('Model Comparison', fontsize=16, fontweight='bold')
+fig.set_figheight(7)
+fig.set_figwidth(14)
+fig.set_facecolor('white')
+
+# First plot
+## set bar size
+barWidth = 0.2
+clf_score = [clf_eval['acc'], clf_eval['prec'], clf_eval['rec'], clf_eval['f1'], clf_eval['kappa']]
+knn_score = [knn_eval['acc'], knn_eval['prec'], knn_eval['rec'], knn_eval['f1'], knn_eval['kappa']]
+
+
+## Set position of bar on X axis
+r1 = np.arange(len(clf_score))
+r2 = [x + barWidth for x in r1]
+
+## Make the plot
+ax1.bar(r1, clf_score, width=barWidth, edgecolor='white', label='Decision Tree')
+ax1.bar(r2, knn_score, width=barWidth, edgecolor='white', label='K-Nearest Neighbors')
+
+## Configure x and y axis
+ax1.set_xlabel('Metrics', fontweight='bold')
+labels = ['Accuracy', 'Precision', 'Recall', 'F1', 'Kappa']
+ax1.set_xticks([r + (barWidth * 1.5) for r in range(len(clf_score))], )
+ax1.set_xticklabels(labels)
+ax1.set_ylabel('Score', fontweight='bold')
+ax1.set_ylim(0, 1)
+
+## Create legend & title
+ax1.set_title('Evaluation Metrics', fontsize=14, fontweight='bold')
+ax1.legend()
+
+# Second plot
+## Comparing ROC Curve
+ax2.plot(clf_eval['fpr'], clf_eval['tpr'], label='Decision Tree, auc = {:0.5f}'.format(clf_eval['auc']))
+ax2.plot(knn_eval['fpr'], knn_eval['tpr'], label='K-Nearest Nieghbor, auc = {:0.5f}'.format(knn_eval['auc']))
+
+## Configure x and y axis
+ax2.set_xlabel('False Positive Rate', fontweight='bold')
+ax2.set_ylabel('True Positive Rate', fontweight='bold')
+
+## Create legend & title
+ax2.set_title('ROC Curve', fontsize=14, fontweight='bold')
+ax2.legend(loc=4)
+
+plt.savefig("./models/model_comparision.jpg")
+
+# Save the model to a file
+with open("./models/heart_disease_classifier.sav", "wb") as f:
+    pickle.dump(knn, f)
+
+app = Flask(__name__)
+model = load(open("./models/heart_disease_classifier.sav", "rb"))
+class_dict = {
+    "0": "No Heart Disease",
+    "1": "Heart Disease",
+    }
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        
+        # Obtain values from form
+        val1 = float(request.form["BMI"])
+        val2 = float(request.form["Smoking"])
+        val3 = float(request.form["AlcoholDrinking"])
+        val4 = float(request.form["Stroke"])
+        val5 = float(request.form["PhysicalHealth"])
+        val6 = float(request.form["MentlHealth"])
+        val7 = float(request.form["DiffWalking"])
+        val8 = float(request.form["Sex"])
+        val9 = float(request.form["Diabetic"])
+        val10 = float(request.form["PhysicalActivity"])
+        val11 = float(request.form["SleepTime"])
+        val12 = float(request.form["Asthma"])
+        val13 = float(request.form["KidneyDisease"])
+        val14 = float(request.form["SkinCancer"])
+
+        
+        data = [[val1, val2, val3, val4, val5, val6, val7, val8, val9, val10, val11, val12, val13, val14]]
+
+        prediction = str(knn.predict(data)[0])
+        pred_class = class_dict[prediction]
+    else:
+        pred_class = None
+    
+    return render_template("index.html", prediction = pred_class)
